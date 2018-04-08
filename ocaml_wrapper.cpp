10b8e1f76b53fbf628cfc2fca1d781e3cad3abb1
Clouds Flowing <clouds.flowing@gmail.com>
2018-04-08 14:48:16 +0800

    split core library

---
+++ ocaml_wrapper.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Clouds Flowing on 4/8/18.
+//
+
+#include "ocaml_wrapper.h"
---
+++ ocaml_wrapper.h
@@ -0,0 +1,8 @@
+//
+// Created by Clouds Flowing on 4/8/18.
+//
+
+#ifndef SYMENGINE_OCAML_WRAPPER_H
+#define SYMENGINE_OCAML_WRAPPER_H
+
+#endif //SYMENGINE_OCAML_WRAPPER_H
