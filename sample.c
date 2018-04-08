585ae8b5fe3704d02605b90059e0c4104331f942
Clouds Flowing <clouds.flowing@gmail.com>
2018-04-08 12:48:03 +0800

    basic_add

--- sample.c
+++ sample.c
@@ -1,13 +1,16 @@
 #include "visitor.h"
 
 int main() {
-  basic x, y;
+  basic x, y, z;
   int t;
   basic_new_stack(x);
   basic_new_stack(y);
+  basic_new_stack(z);
   symbol_set(x, "x");
   basic_cos(y, x);
-  print_ast(y);
+  basic_add(z, y, x);
+  basic_add(z, z, y);
+  print_ast(z);
   basic_free_stack(x);
   basic_free_stack(y);
   return 0;
